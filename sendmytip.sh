f1c6bd99381f32ca04ae435422ca6344b9eb5e81
Mike <mike@fullman.net>
2020-01-02 00:21:30 -0800

    update for last hash and last pool id

--- sendmytip.sh
+++ sendmytip.sh
@@ -7,9 +7,13 @@ THIS_GENESIS="8e4d2a343f3dcf93"   # We only actually look at the first 7 charact
 
 if [ ! $JORMUNGANDR_RESTAPI_URL ]; then export JORMUNGANDR_RESTAPI_URL=http://127.0.0.1:${RESTAPI_PORT}/api; fi
 alias cli="$(which jcli) rest v0"
+nodestats=$(cli node stats get --output-format json > stats.json);
 
-lastBlockHeight=$(cli node stats get --output-format json | jq -r .lastBlockHeight)
-echo ${lastBlockHeight}
+lastBlockHeight=$(cat stats.json | jq -r .lastBlockHeight)
+lastBlockHash=$(cat stats.json | jq -r .lastBlockHash)
+lastPoolID=$(cli block ${lastBlockHash} get | cut -c169-232)
+echo $lastblock
+echo   "https://api.pooltool.io/v0/sharemytip?poolid=${MY_POOL_ID}&userid=${MY_USER_ID}&genesispref=${THIS_GENESIS}&mytip=${lastBlockHeight}&lasthash=${lastBlockHash}&lastpool=${lastPoolID}"
 if [ "$lastBlockHeight" != "" ]; then
-curl -G "https://api.pooltool.io/v0/sharemytip?poolid=${MY_POOL_ID}&userid=${MY_USER_ID}&genesispref=${THIS_GENESIS}&mytip=${lastBlockHeight}"
+curl -G "https://api.pooltool.io/v0/sharemytip?poolid=${MY_POOL_ID}&userid=${MY_USER_ID}&genesispref=${THIS_GENESIS}&mytip=${lastBlockHeight}&lasthash=${lastBlockHash}&lastpool=${lastPoolID}"
 fi
