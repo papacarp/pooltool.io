e4fa681b7abeaa8c4b4a1eb4dc61a89a61c1d07c
Ubuntu <ubuntu@ip-172-31-32-11.us-west-2.compute.internal>
2020-01-29 06:58:56 +0000

    lastparent, lastslot, lastepoch, platform, jormver added to stat submissions

--- sendmytip.sh
+++ sendmytip.sh
@@ -1,19 +1,25 @@
 #!/bin/bash
 shopt -s expand_aliases
 RESTAPI_PORT=5001
-MY_POOL_ID="52b33axxxxxxxxxxxx"
-MY_USER_ID="xxxxxxx-xxxx-xxxx-xxxx-xxxxxxxxxxx" # on pooltool website get this from your account profile page
+# MY_POOL_ID="52b33axxxxxxxxxxxx"
+# MY_USER_ID="xxxxxxx-xxxx-xxxx-xxxx-xxxxxxxxxxx" # on pooltool website get this from your account profile page
 THIS_GENESIS="8e4d2a343f3dcf93"   # We only actually look at the first 7 characters
 
 if [ ! $JORMUNGANDR_RESTAPI_URL ]; then export JORMUNGANDR_RESTAPI_URL=http://127.0.0.1:${RESTAPI_PORT}/api; fi
 alias cli="$(which jcli) rest v0"
 nodestats=$(cli node stats get --output-format json > stats.json);
-
+platformName="sendmytip.sh"
 lastBlockHeight=$(cat stats.json | jq -r .lastBlockHeight)
-lastBlockHash=$(cat stats.json | jq -r .lastBlockHash)
-lastPoolID=$(cli block ${lastBlockHash} get | cut -c169-232)
+jormVersion=$(cat stats.json | jq -r .version)
 
-echo   "https://api.pooltool.io/v0/sharemytip?poolid=${MY_POOL_ID}&userid=${MY_USER_ID}&genesispref=${THIS_GENESIS}&mytip=${lastBlockHeight}&lasthash=${lastBlockHash}&lastpool=${lastPoolID}"
+lastBlockHash=$(cat stats.json | jq -r .lastBlockHash)
+lastBlock=$(cli block ${lastBlockHash} get)
+lastPoolID=${lastBlock:168:64}
+lastParent=${lastBlock:104:64}
+lastSlot=$((0x${lastBlock:24:8}))
+lastEpoch=$((0x${lastBlock:16:8}))
+
 if [ "$lastBlockHeight" != "" ]; then
-curl -G "https://api.pooltool.io/v0/sharemytip?poolid=${MY_POOL_ID}&userid=${MY_USER_ID}&genesispref=${THIS_GENESIS}&mytip=${lastBlockHeight}&lasthash=${lastBlockHash}&lastpool=${lastPoolID}"
+curl -G --data-urlencode "platform=${platformName}" --data-urlencode "jormver=${jormVersion}" "https://api.pooltool.io/v0/sharemytip?poolid=${MY_POOL_ID}&userid=${MY_USER_ID}&genesispref=${THIS_GENESIS}&mytip=${lastBlockHeight}&lasthash=${lastBlockHash}&lastpool=${lastPoolID}&lastparent=${lastParent}&lastslot=${lastSlot}&lastepoch=${lastEpoch}"
 fi
+
