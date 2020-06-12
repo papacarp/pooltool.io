e26ec3c513d1aa3f066ac5917a0146770b52724c
papacarp <mikef@zenshows.com>
2020-06-12 11:35:36 -0700

    save topology

--- buddies/get_buddies.sh
+++ buddies/get_buddies.sh
@@ -6,8 +6,18 @@ MY_POOL_ID="XXXXXXXX"
 MY_API_KEY="XXXXXXXX"
 ## GET THIS FROM YOUR POOL MANAGE TAB ON POOLTOOL WEBSITE
 MY_NODE_ID="XXXXXXXX"
+## SET THIS TO THE LOCATION OF YOUR TOPOLOGY FILE THAT YOUR NODE USES
+TOPOLOGY_FILE="$CNODE_HOME/files/ff-topology-buddies.json"
 
-## IF WE GET TO HERE THEN NEITHER VERIFICATION METHOD IS BEING USED. JUST SEND CURRENT SLOTS
 JSON="$(jq -n --compact-output --arg MY_API_KEY "$MY_API_KEY" --arg MY_POOL_ID "$MY_POOL_ID" --arg MY_NODE_ID "$MY_NODE_ID" '{apiKey: $MY_API_KEY, nodeId: $MY_NODE_ID, poolId: $MY_POOL_ID}')"
 echo "Packet Sent: $JSON"
-echo "Response Received: $(curl -s -H "Accept: application/json" -H "Content-Type:application/json" -X POST --data "$JSON" "https://api.pooltool.io/v0/getbuddies")"
+RESPONSE="$(curl -s -H "Accept: application/json" -H "Content-Type:application/json" -X POST --data "$JSON" "https://api.pooltool.io/v0/getbuddies")"
+SUCCESS="$(echo $RESPONSE | jq '.success')"
+if [ $SUCCESS ]; then
+  echo "Success"
+  echo $RESPONSE | jq '. | {Producers: .message}' > $TOPOLOGY_FILE
+  echo "Topology saved to $TOPOLOGY_FILE.  Note topology will only take effect next time you restart your node"
+else
+  echo "Failure "
+  echo $RESPONSE | jq '.message'
+fi
