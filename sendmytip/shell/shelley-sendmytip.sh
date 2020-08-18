85a45095e9e1fd645677b805e4eb7abe1a699b27
Ubuntu <ubuntu@ip-172-31-7-8.us-west-2.compute.internal>
2020-08-18 04:36:24 +0000

    add platform and other minor tweaks

--- sendmytip/shell/shelley-sendmytip.sh
+++ sendmytip/shell/shelley-sendmytip.sh
@@ -9,7 +9,7 @@ PT_MY_API_KEY="xxxx-xx-xx-xx-xxxx"
 # Your node ID (optional, this is reserved for future use and is not captured yet)
 PT_MY_NODE_ID="xxxx-xxx-xxx-xxx-xxxx"
 # THE NAME OF THE SCRIPT YOU USE TO MANAGE YOUR POOL
-PLATFORM="sendmytip-haskell.sh"
+PLATFORM="shelley-sendmytip.sh"
 # The location of your log file.
 # EXAMPLE CONFIGURATION ENTRIES IN YOUR config.json FILE FOR YOUR NODE:
 # "defaultScribes": [
@@ -28,6 +28,7 @@ PLATFORM="sendmytip-haskell.sh"
 #    }
 # ]
 
+# MODIFY THIS LINE TO POINT TO YOUR LOG FILE AS SPECIFIED IN YOUR CONFIG FILE
 LOG_FILE="/opt/cardano/cnode/logs/node-0.json"
 
 SOCKET="/opt/cardano/cnode/sockets/node0.socket"
@@ -37,29 +38,24 @@ export CARDANO_NODE_SOCKET_PATH=${SOCKET}
 shopt -s expand_aliases
 
 alias cli="$(which cardano-cli) shelley"
-alias node="$(which cardano-node)"
-platformName="sendmytip_haskell"
 
 tail -Fn0 $LOG_FILE | \
 while read line ; do
     echo "$line" | grep "TraceAddBlockEvent.AddedToCurrentChain" 2>/dev/null
     if [ $? = 0 ]
     then
-        # current cardano-node json output has a bug in it with an extra quote it seems.  so for now we just extract by string position rather than jq  ("newtip":"\"8afc7f@6131589")
-        #AT=$(echo ${line} | cut -c8-30)
+        # current cardano-node json output has a bug in it with an extra quote it seems so jq doesn't work by default.  ("newtip":"\"8afc7f@6131589")
         # Fix the line so that it's proper JSON
         LINE=$(echo "$line" | sed "s/\"newtip\":\"\"/\"newtip\":\"/g")
-
         nodeVersion=$(echo ${LINE} | jq -r .env)
-        AT=$(echo ${LINE} | jq -r .data.newtip)
+        AT=$(echo ${LINE} | jq -r .at)
 
         nodeTip=$(cli query tip --mainnet)
         lastSlot=$(echo ${nodeTip} | jq -r .slotNo)
         lastBlockHash=$(echo ${nodeTip} | jq -r .headerHash)
         lastBlockHeight=$(echo ${nodeTip} | jq -r .blockNo)
 
-        #JSON="$(jq -n --compact-output --arg NODE_ID "$PT_MY_NODE_ID" --arg MY_API_KEY "$PT_MY_API_KEY" --arg MY_POOL_ID "$PT_MY_POOL_ID" --arg VERSION "$nodeVersion" --arg AT "$AT" --arg BLOCKNO "$lastBlockHeight" --arg SLOTNO "$lastSlot" --arg BLOCKHASH "$lastBlockHas$
-        JSON="$(jq -n --compact-output --arg NODE_ID "$PT_MY_NODE_ID" --arg MY_API_KEY "$PT_MY_API_KEY" --arg MY_POOL_ID "$PT_MY_POOL_ID" --arg VERSION "$nodeVersion" --arg AT "$AT" --arg BLOCKNO "$lastBlockHeight" --arg SLOTNO "$lastSlot" --arg PLATFORM "$platformName" --arg BLOCKHASH "$lastBlockHash" '{apiKey: $MY_API_KEY, poolId: $MY_POOL_ID, data: {nodeId: $NODE_ID, version: $VERSION, at: $AT, blockNo: $BLOCKNO, slotNo: $SLOTNO, blockHash: $BLOCKHASH, platform: $PLATFORM}}')"
+        JSON="$(jq -n --compact-output --arg NODE_ID "$PT_MY_NODE_ID" --arg MY_API_KEY "$PT_MY_API_KEY" --arg MY_POOL_ID "$PT_MY_POOL_ID" --arg VERSION "$nodeVersion" --arg AT "$AT" --arg BLOCKNO "$lastBlockHeight" --arg SLOTNO "$lastSlot" --arg PLATFORM "$PLATFORM" --arg BLOCKHASH "$lastBlockHash" '{apiKey: $MY_API_KEY, poolId: $MY_POOL_ID, data: {nodeId: $NODE_ID, version: $VERSION, at: $AT, blockNo: $BLOCKNO, slotNo: $SLOTNO, blockHash: $BLOCKHASH, platform: $PLATFORM}}')"
         echo "Packet Sent: $JSON"
 
         if [ "${lastBlockHeight}" != "" ]; then
