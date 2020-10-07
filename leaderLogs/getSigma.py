ae5d81e566b32e9b2ea28eb1a5a29987c50419d0
Crypto2099, Corp <adam@crypto2099.io>
2020-10-07 23:57:09 +0000

    Adding getSigma.py to allow for easy and quick sigma calculation

---
+++ leaderLogs/getSigma.py
@@ -0,0 +1,65 @@
+import json
+import argparse
+import os.path
+from os import path
+
+parser = argparse.ArgumentParser(description="Calculate the sigma value of the specified pool. If you do not provide the path to a ledger file via the --ledger option then the script will look for a ledger.json file in the current directory")
+parser.add_argument('--pool-id', dest='id', help='the pool ID', required=True)
+parser.add_argument('--ledger', dest='ledger', default='ledger.json', help='the path to a current ledger-state JSON file')
+
+args = parser.parse_args()
+
+poolid = args.id
+ledger = args.ledger
+
+if not path.exists(ledger):
+    print("We tried but could not locate your ledger-state JSON file!")
+    print("Use: \033[1;34mcardano-cli shelley query ledger-state --mainnet --out-file ledger.json\033[0m to export one!")
+    exit()
+
+with open(ledger) as f:
+    ledger = json.load(f)
+
+def getSigma(poolId):
+    blockstakedelegators={}
+    blockstake={}
+    bs={}
+    print("building active stake")
+    for item2 in ledger['esSnapshots']["_pstakeSet"]['_delegations']:
+        keyhashobj = []
+        for itemsmall in item2:
+            if 'key hash' in itemsmall:
+
+                keyhashobj.append(itemsmall['key hash'])
+            else:
+                poolid = itemsmall
+        if poolid not in blockstakedelegators:
+            blockstakedelegators[poolid]=keyhashobj
+        else:
+            blockstakedelegators[poolid]=blockstakedelegators[poolid]+keyhashobj
+
+    for item2 in ledger['esSnapshots']["_pstakeSet"]['_stake']:
+        delegatorid = None
+        for itemsmall in item2:
+            if isinstance(itemsmall,int):
+                snapstake = itemsmall
+            else:
+                delegatorid=itemsmall['key hash']
+        if delegatorid != None:
+            if delegatorid not in blockstake:
+                blockstake[delegatorid]=snapstake
+            else:
+                blockstake[delegatorid]=blockstake[delegatorid]+snapstake
+    total_bs=0
+    for poolid in blockstakedelegators:
+        bs[poolid]=0
+        for d in blockstakedelegators[poolid]:
+            if d in blockstake:
+                bs[poolid]=bs[poolid]+blockstake[d]
+                total_bs=total_bs + blockstake[d]
+
+
+    return float(bs[poolId]/total_bs)
+
+
+print(getSigma(poolid))
